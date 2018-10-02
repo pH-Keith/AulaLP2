dde0661f328e9de92c8ae71b45668049a0029e85
António <tonny_ctl@hotmail.com>
2018-10-02 16:55:55 +0100

    First Commit.

---
+++ MyAula01/MyFortnite/FNPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFortnite
+{
+    class FNPlayer
+    {
+        public int ammo = 10;
+        public float health;
+
+        public float Attack(FNPlayer enemy)
+        {
+            float damage = 10;
+            enemy.health -= damage;
+            ammo--;
+            return damage;
+        }
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+        }
+    }
+}
